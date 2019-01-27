27377ce034c350682aba9ae49bccd5ab19d0dc28
SleepingFox <seanloganking@gmail.com>
2019-01-26 21:06:48 -0400

    Delete dns.js

--- test/dns.js
+++
@@ -1,8 +0,0 @@
-const DNS = artifacts.require("DNS");
-
-contract("DNS", accounts => {
-  interface("Should claim a name", function(){
-    const instance = await DNS.deployed();
-    assert.equal(1,1, "should have 1");
-  });
-});
